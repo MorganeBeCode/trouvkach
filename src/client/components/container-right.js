172beef457d523f17835e8c9f2071a7a01e00b1f
Dumont Augustin <augustindmt@gmail.com>
2019-12-09 17:02:49 +0100

    merge branche morgane dev + components front-end Gus

--- src/client/components/container-right.js
+++ src/client/components/container-right.js
@@ -1,11 +1,9 @@
 import React from "react";
 import LeafletMap from "./leaflet-map";
 
-const ContainerRight = () => {
-    return (
-        <div className={"container-right"}>
-            <LeafletMap />
-        </div>
-    );
-};
+const ContainerRight = () => (
+    <div className={"container-right"}>
+        <LeafletMap />
+    </div>
+);
 export default ContainerRight;
--- src/client/components/research.js
+++ src/client/components/research.js
@@ -1,28 +1,25 @@
 import React from "react";
 
-const Research = () => {
+const Research = () => (
     // const handleSubmit = event => {
     //     event.preventDefault();
     //     const researchUser = {input};
     //     // utiliser le input dans un find() de l'objet API
     // };
-    return (
-        // onSubmit={handleSubmit} (à insérer dans balise forme)
-        <form>
-            <input
-                type={"text"}
-                placeholder={"Research an ATM"}
-                // value={"input"}
+    // onSubmit={handleSubmit} (à insérer dans balise forme)
+    <form>
+        <input
+            type={"text"}
+            placeholder={"Research an ATM"}
+            // value={"input"}
+        />
+        <button type={"submit"}>
+            <img
+                src={"https://svgsilh.com/svg/1093184-00bcd4.svg"}
+                width={"24px"}
+                height={"auto"}
             />
-            <button type={"submit"}>
-                <img
-                    src={"https://svgsilh.com/svg/1093184-00bcd4.svg"}
-                    width={"24px"}
-                    height={"auto"}
-                />
-            </button>
-        </form>
-    );
-};
-
+        </button>
+    </form>
+);
 export default Research;
